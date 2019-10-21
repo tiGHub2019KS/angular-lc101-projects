bc8f8c1aae7b34f65b0725f0033c34417dab237b
Kathy <kvsippo@charter.net>
2019-10-20 23:07:37 -0500

    this should be Angular Lesson 1 Part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://vignette.wikia.nocookie.net/disney/images/0/01/Toy_Story_4_first_look.png/revision/latest/scale-to-width-down/185?cb=20181112151309';
+  image3 = 'https://image.shutterstock.com/image-photo/green-shopping-bag-grocery-products-260nw-144819322.jpg';
 
   constructor() { }
 
